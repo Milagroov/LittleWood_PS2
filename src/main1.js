e155394cb475588c74a4eca5a4203d02142d8a2d
Romain PUECH <romain.puech691@gmail.com>
2022-04-04 13:50:51 +0200

    setup complet

---
+++ src/main1.js
@@ -0,0 +1,15 @@
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    backgroundColor: '#ffffff',
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 100 }
+        }
+    },
+    scene: new Tableau1()
+};
+let game = new Phaser.Game(gameConfig);
