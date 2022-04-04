34dd3e2fa8342402cf3f1fbd3a81fc38776debe9
Romain PUECH <romain.puech691@gmail.com>
2022-04-04 15:47:58 +0200

    start player + cam + bg

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,7 +1,23 @@
 class Tableau1 extends Phaser.Scene{
+
     preload() {
 
         this.load.image('background', 'assets/images/background.png');
-        
+        this.load.spritesheet('player', 'assets/images/player.png', {frameWidth: 48, frameHeight: 48});
     }
+
+    create() {
+
+        this.bg1 = this.add.image(0, 0, 'background').setOrigin(0, 0);
+
+        this.player = this.physics.add.sprite(200, 100, "player")
+        this.player.flipX = true;
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.player.setDepth(0);
+
+        this.cameras.main.startFollow(this.player);
+    }
+
+
 }
