3e13a2053a854be0323c196080e0059263631814
Romain PUECH <92451537+Milagroov@users.noreply.github.com>
2022-05-20 11:23:02 +0200

    push setup not working

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -2,13 +2,30 @@ class Tableau1 extends Phaser.Scene{
 
     preload() {
 
-        this.load.image('background', 'assets/images/background.png');
+        this.load.image('fond1', 'assets/images/fond1.png');
+        this.load.image('fond2', 'assets/images/fond2.png');
+        this.load.image('fond3', 'assets/images/fond3.png');
+        this.load.image('fond4', 'assets/images/fond4.png');
+
+        this.load.image('tiles', 'assets/tilesets/tileset.png');
+        this.load.image('ronce', 'assets/tilesets/ronces.png');
+        this.load.image('ronce', 'assets/tilesets/nuage.png');
+        this.load.image('ronce', 'assets/tilesets/nuage_magique.png');
+
+
         this.load.spritesheet('player', 'assets/images/player.png', {frameWidth: 48, frameHeight: 48});
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.tmj');
+
     }
 
     create() {
 
-        this.bg1 = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        this.bg1 = this.add.image(0, 0, 'fond1').setOrigin(0, 0);
+
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('tileset', 'tiles');
+        const terre = map.createStaticLayer('terre', tileset, 0, 200);
+        terre.setCollisionByExclusion(-1, true);
 
         this.player = this.physics.add.sprite(200, 100, "player")
         this.player.flipX = true;
